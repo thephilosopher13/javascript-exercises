5ecd7be406d2b4aea9b33e4fdee55756447a77b1
Coloqs <60373194+thephilosopher13@users.noreply.github.com>
2023-05-24 17:13:42 +0800

    Complete exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,20 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+   let heyString = "";
+   if (num < 0) {
+    heyString += 'ERROR';
+   } else {
+    for (i = 1; i <= num; i++) {
+        {
+            heyString += string;
+        }
+       }
+   }
+   return heyString;
 };
+
+repeatString('hey', 10)
+
+repeatString('hey', -1)
 
 // Do not edit below this line
 module.exports = repeatString;
