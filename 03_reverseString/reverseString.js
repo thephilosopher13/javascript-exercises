bc2498a4d20224e23cf70143144fe1b62aee5811
Coloqs <60373194+thephilosopher13@users.noreply.github.com>
2023-05-24 17:27:19 +0800

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    var reversedString = ""
+    for (i = (string.length-1) ; i >= 0; i--) {
+        const stringSlicer = string.slice(i, i + 1)
 
+        reversedString += stringSlicer
+    }
+    return reversedString
 };
 
 // Do not edit below this line
