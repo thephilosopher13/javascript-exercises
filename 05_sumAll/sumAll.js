c4842fd6da385ea8c1febe42b4dcb8eca49d6094
Coloqs <60373194+thephilosopher13@users.noreply.github.com>
2023-05-24 18:22:41 +0800

    Finish Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
-
-};
-
+const sumAll = function(num1, num2) {
+   var sum = 0;
+   const isNotNumber = function(value) {
+      return typeof value !== 'number' || Number.isNaN(value);
+   };
+   if (num1 < 0 || num2 < 0 || isNotNumber(num1) || isNotNumber(num2)) {
+     sum = 'ERROR';
+   } else if (num1 <= num2) {
+      for (let i = num1; i <= num2; i++) {
+        sum = sum + i;
+      }
+    } else {
+      for (let i = num2; i <= num1; i++) {
+        sum = sum + i;
+      }
+    }
+   return sum;
+ };
+ 
 // Do not edit below this line
 module.exports = sumAll;
