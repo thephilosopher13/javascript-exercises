1ed24ee8d839363ce25730de2238a1a8b50f449f
Coloqs <60373194+thephilosopher13@users.noreply.github.com>
2023-05-28 00:30:26 +0800

    FInish exercise 10 and 11

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    const num = Number(n);
+    const fibonacciArray = [0, 1];
+
+
+    for (let i = 2; i <= num; i++) {
+        fibonacciArray.push((fibonacciArray[i-1] + (fibonacciArray[i-2])));
+    }
+
+    if (n < 0) {
+        return 'OOPS';
+    } else {
+        return fibonacciArray[num]
+    }
+
 
 };
 
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    const titleArray = []
+    for (let key in arr) {
+        var value = arr[key].title
+        titleArray.push(value)
+    }
+    return titleArray
 };
 
+console.log(getTheTitles)
 // Do not edit below this line
 module.exports = getTheTitles;
