8a46d1d36ab28d6c375b000a0d987c6eeac439e8
Coloqs <60373194+thephilosopher13@users.noreply.github.com>
2023-05-27 22:11:23 +0800

    Finish Exercise 08

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+   let convertedValue = ((value - 32)*0.556);
+   let roundedValue = Number(convertedValue.toFixed(1));
+   return roundedValue
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+  let convertedValue = ((value*1.8) + 32)
+  let roundedValue = Number(convertedValue.toFixed(1))
+  return roundedValue
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,36 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return (a + b);
 };
 
-const subtract = function() {
-	
+
+const subtract = function(a,b) {
+	return (a >= b ? (a - b): b - a);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	if (Array.isArray(arr)) {
+    return arr.reduce((sum, current) => sum + current, 0);
+  } 
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (Array.isArray(arr)) {
+    return arr.reduce((multiply, current) => multiply * current);
+  } 
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return (a ** b);
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  let factorialResult = 1
+
+  for (let i = 2; i <= n; i++) {
+    factorialResult *= i
+  }
+
+  return factorialResult
 	
 };
 
@@ -31,3 +43,5 @@ module.exports = {
   power,
   factorial
 };
+
+add.reduce 
