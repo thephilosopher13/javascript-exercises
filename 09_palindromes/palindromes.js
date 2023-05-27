8b5608c5b8d580767a2d4a558d4b3f6707710cca
Coloqs <60373194+thephilosopher13@users.noreply.github.com>
2023-05-27 23:55:29 +0800

    Finish Exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,39 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+    return processedString.split("").reverse().join("") == processedString;
+  };
 
-};
+/* 
+
+my original solution but way way fucking longer
+
+const palindromes = function (str) {
+    const stringLettersNumbersOnly = str.replace(/[^\w]|_/g, '');
+    const stringLowerCased = stringLettersNumbersOnly.toLowerCase()
+    const halfLength = ((stringLowerCased.length)/2)
+    
+    function reverseString(a) {
+        return a.split('').reverse().join('');
+      }
+
+    if (stringLettersNumbersOnly.length % 2 === 0) {
+            var firstHalf = stringLowerCased.substring(0, halfLength)
+            var secondHalf = stringLowerCased.substring(halfLength)
+    } else {
+            var firstHalf = stringLowerCased.substring(0, (Math.ceil(halfLength)))
+            var secondHalf = stringLowerCased.substring(Math.floor(halfLength))
+    }
+
+    var secondHalfReversed = reverseString(secondHalf)
+
+    if (firstHalf === secondHalfReversed) {
+        return true; 
+    } else {
+        return false;
+    }
+}; */
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
